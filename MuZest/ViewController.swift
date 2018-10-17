73fd4c02de208d018ef6fa4a9147e41c6b5d34c3
Tumanov Nikita <2manov97@gmail.com>
2018-10-18 02:00:35 +0300

    add project

---
+++ MuZest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MuZest
+//
+//  Created by Никита Туманов on 18/10/2018.
+//  Copyright © 2018 Никита Туманов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MuZestTests/MuZestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MuZestTests.swift
+//  MuZestTests
+//
+//  Created by Никита Туманов on 18/10/2018.
+//  Copyright © 2018 Никита Туманов. All rights reserved.
+//
+
+import XCTest
+@testable import MuZest
+
+class MuZestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
