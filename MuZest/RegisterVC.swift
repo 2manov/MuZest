53f8dfc7be6e8435e551c4cd1620ee474e106c70
Tumanov Nikita <2manov97@gmail.com>
2018-10-18 18:32:59 +0300

    change pass

---
+++ MuZest/RegisterVC.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterVC.swift
+//  MuZest
+//
+//  Created by Никита Туманов on 18/10/2018.
+//  Copyright © 2018 Никита Туманов. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
