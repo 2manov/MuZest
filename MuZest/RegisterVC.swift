925bdb0c39d16220201c9c9962b0214ef111fcd3
Ivan Vozvakhov <vozvivan@gmail.com>
2018-10-20 22:43:15 +0300

    Add simpliest registration

--- MuZest/RegisterVC.swift
+++ MuZest/RegisterVC.swift
@@ -7,16 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterVC: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordConfirm: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func registerAction(_ sender: Any) {
+        if password.text != passwordConfirm.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "toHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
